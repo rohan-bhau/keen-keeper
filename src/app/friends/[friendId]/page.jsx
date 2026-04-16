d1be25be897f380314c0543e1636f1de56f225da
Rohan Mia <rohanmia.org@gmail.com>
2026-04-16 23:11:45 +0600

    commit

--- src/app/friends/[friendId]/page.jsx
+++ src/app/friends/[friendId]/page.jsx
@@ -1,17 +1,18 @@
-import DetailsCard from '@/ui/FriendDetailCard/DetailsCard';
-import React from 'react'
+export const dynamic = "force-dynamic";
+import DetailsCard from "@/ui/FriendDetailCard/DetailsCard";
+import React from "react";
 
 const FriendDetailPage = async ({ params }) => {
-    const { friendId } = await params;
+  const { friendId } = await params;
 
   return (
     <div>
       <DetailsCard friendId={friendId}></DetailsCard>
     </div>
-  )
-}
+  );
+};
 
-export default FriendDetailPage
+export default FriendDetailPage;
 
 // export default function FriendDetailPage(props) {
 //   console.log("PROPS:", props);
