1d0ecb219d577b7aa34459fe573cd3a78e012054
Rohan Mia <rohanmia.org@gmail.com>
2026-04-16 22:07:20 +0600

    fix: friend details lookup and reload issue on deployment

--- src/app/friends/[friendId]/page.jsx
+++ src/app/friends/[friendId]/page.jsx
@@ -3,12 +3,18 @@ import React from 'react'
 
 const FriendDetailPage = async ({ params }) => {
     const { friendId } = await params;
-   
+
   return (
     <div>
-      <DetailsCard friendId={parseInt(friendId)}></DetailsCard>
+      <DetailsCard friendId={friendId}></DetailsCard>
     </div>
   )
 }
 
 export default FriendDetailPage
+
+// export default function FriendDetailPage(props) {
+//   console.log("PROPS:", props);
+
+//   return <div>Check console</div>;
+// }
--- src/ui/FriendDetailCard/DetailsCard.jsx
+++ src/ui/FriendDetailCard/DetailsCard.jsx
@@ -4,9 +4,11 @@ import LeftCard from "./LeftCard";
 import RightCard from "./RightCard";
 
 const DetailsCard = ({ friendId }) => {
+  // console.log(typeof friendId);
   const {friends,loading} = useData()
-  const friend = friends.find(fr => fr.id === friendId)
-  // console.log(friend)
+  const friend = friends.find((fr) => Number(fr.id) === Number(friendId));
+  // console.log(typeof (friend.id));
+
   // console.log(friends, loading);
   return (
     <div className="container mx-auto py-15">
@@ -14,10 +16,15 @@ const DetailsCard = ({ friendId }) => {
         <div className="flex justify-center items-center">
           <span className="loading loading-spinner text-error"></span>
         </div>
-      ) : (
+      ) : friend ? ( 
         <div className="flex flex-col md:flex-row gap-6 px-20 ">
           <LeftCard friend={friend}></LeftCard>
           <RightCard friend={friend}></RightCard>
+        </div>
+      ) : (        
+        <div className="text-center py-20">
+          <h2 className="text-2xl font-bold">Friend not found!</h2>
+          <p>ID: {friendId}</p>
         </div>
       )}
     </div>
